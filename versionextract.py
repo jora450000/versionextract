9e46a2057a92fc4039bd34aec6726b8a53ac098e
Eugene <adm@eugene.bars.group>
2020-08-07 12:17:11 +0300

    Changes to be committed: 	modified:   out1.csv 	modified:   versionextract.py
    fixed:
    	sirings with shot version format

--- versionextract.py
+++ versionextract.py
@@ -2,17 +2,29 @@ import re
 
 regex = r'''(\d+\.\d+\.\d+\-\d+)'''
 
-#print(re.findall(regex, str)[0])  # prints ['1.4.2.4']
+regex_short1 = r'''(\d+\.\d+\-\d+)'''
+
+regex_short2 = r'''(\d+\-\d+)'''
+
 
 f = open('in.csv')
 
 line = f.readline()
 while line:
     subs = re.split(regex, line)
+    if (len(subs) < 2):
+        subs = re.split(regex_short1, line)
+    if (len(subs) < 2):
+        subs = re.split(regex_short2, line)
+
+
+
     if (len(subs) == 2):
         print(f"{line[:-1]},  {subs[0][:-1]}, {subs[1]}")
     elif (len(subs) == 3):
         print(f"{line[:-1]},  {subs[0][:-1]},  {subs[1]}{subs[2][:-1]}")
+    else:
+        print(f"{line[:-1]},  {subs[0][:-1]}, ???")
     line = f.readline()
 
 
